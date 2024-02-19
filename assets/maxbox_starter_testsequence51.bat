7d75cee24913b59d7e6815a7bed5bf91e2573488
Max Kleiner <max@kleiner.com>
2024-02-19 16:33:37 +0100

    Add files via upload

---
+++ assets/maxbox_starter_testsequence51.bat
@@ -0,0 +1,20 @@
+REM run the script engine all with c
+REM maxbox5.exe "examples\044_queens_performer3bat.txt" para2
+maxbox5.exe -c "examples\1013_saint_source23tests_64.txt" 
+maxbox5.exe -c "examples\26_11_ibz_virus3_wmiserv64.txt"
+maxbox5.exe -c "examples\714_astroids_GAME5_64.PAS" 
+maxbox5.exe -c "examples\965_RSA_IBZ_PrivatePublicKey_Form4p_eng64.pas" 
+maxbox5.exe -c "examples\165_best_of_runtime464.txt" 
+maxbox5.exe -c "examples\1176_APILayer_Demo64.txt" 
+maxbox5.exe -c "examples\262_mxoutputdemo5.txt" 
+maxbox5.exe -c "examples\1016_sentiment_api2_bbc_newsfeed64.txt" 
+maxbox5.exe -c "examples\826_sentiment_api2_2_64.txt"
+maxbox5.exe -c "examples\139_pas_regex3securitybymelani64.TXT" 
+maxbox5.exe -c "examples\521_iputils2_64.txt" 
+maxbox5.exe -c "examples\743_ProperDivisor64.txt" 
+REM maxbox4.exe "examples\419_archimedes_spiral2.txt" 
+maxbox5.exe -c "examples\cannonball64_2.txt" 
+maxbox5.exe -c "examples\778_advapi32_dll_SHA256_64.txt" 
+PAUSE
+REM Terminate
+
